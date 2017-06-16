010a0f73f124d9abd646d5177a650db8d08dc8a6
airtoxin <airtoxin@icloud.com>
2017-06-16 11:56:11 +0900

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("@1", 1);
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = (
+  <Provider store={store}>
+    <h1>hello</h1>
+  </Provider>
+);
+
+ReactDom.render(
+  App,
+  document.getElementById('app'),
+);
